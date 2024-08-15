a2dc4b709e97186a74475e6fd05217c9a198e4ac
Shubham Rane <shubhamrane016@gmail.com>
2024-08-15 10:49:05 +0530

    Updated Advanced Patterns

--- patterns.java
+++ patterns.java
@@ -130,6 +130,26 @@ public class patterns {
     //     mirror_pyramid(5);
     // }
 
+    //Square number pattern
+
+    // 1 2 3 4 5 
+    // 2 3 4 5 6 
+    // 3 4 5 6 7 
+    // 4 5 6 7 8 
+    // 5 6 7 8 9
+    
+    public static void SqNo_Pattern(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i+j-1 + " ");
+            }
+            System.out.println();
+        }
+    }
+    // public static void main(String[] args) {
+    //     SqNo_Pattern(5);
+    // }
+
     //Inverted Half-Pyramid with numbers
 
  // 1 2 3 4 5 
